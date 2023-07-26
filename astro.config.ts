7482b43f5d2594f168e8be388444b0c207003bb2
0xYami <0xYami@proton.me>
2023-07-27 00:33:40 +0200

    feat: add `vercel` adapter

--- astro.config.ts
+++ astro.config.ts
@@ -5,9 +5,14 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel({
+    analytics: true,
+  }),
+  output: "server",
   site: "https://yami.sh",
   integrations: [prefetch(), sitemap(), mdx(), react(), svelte(), tailwind()],
   markdown: {
